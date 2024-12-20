a911056bf5629f33282e2a220534cabc164adb23
Elijah Wood <ej.thewoodies@gmail.com>
2024-12-20 10:40:10 +0000

    Database User Setup

---
+++ src/js/newUser.js
@@ -0,0 +1,16 @@
+/* fetch("http://localhost:3000/register", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ username: "testUser", password: "testPass" }),
+})
+  .then((res) => res.json())
+  .then((data) => {
+    if (data.token) {
+      console.log("Login successful:", data.token);
+    } else {
+      console.log(data.error);
+    }
+  });
+
+console.log("Newuser.js executed!");
+*/
